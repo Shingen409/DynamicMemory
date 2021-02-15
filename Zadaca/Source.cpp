4fb35a2d717de956f234510542b75d3806cfdafe
Kano_rn99@itstepru.onmicrosoft.com <Kano_rn99@itstepru.onmicrosoft.com>
2021-02-15 23:10:21 +0500

    Novaya zadaca

---
+++ Zadaca/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Задача:\nСколько нужно праграммистов чтобы вкрутить лампочку?:" << endl;
+
+
+
+}
